9e5d6333a5f521c25ad1bc413633ee136f95bbbc
Reinms <reinmasamuneytstuff@gmail.com>
2020-12-26 17:30:17 -0500

    Added token access from handle

--- Rein.Core.RegistrySystem/RegistryHandle.cs
+++ Rein.Core.RegistrySystem/RegistryHandle.cs
@@ -7,5 +7,6 @@
         private protected abstract Registry target { get; }
 
         internal Boolean initCompleted => target._stage == Stage.Finalized;
+        internal MetaRegistry.RegistrationToken? regToken => target.regToken;
     }
 }
